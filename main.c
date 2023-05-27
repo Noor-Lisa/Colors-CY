fca3fe70a8e47e549057b23343a75e08d2204563
Noor-Lisa <130580271+Noor-Lisa@users.noreply.github.com>
2023-05-27 23:29:51 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,1 @@
-# Colors-CY
-#include <stdio.h>
-#include <stdlib.h>
-#include "colors.c"
-#include "Board.c"
 
-int main(){
-printf("Bravo les filles vous avez trouvé le fichier.\n");
-printf("Hello world !");
-printf("coucouuuu");
-mon commit
-return 0;
-}
